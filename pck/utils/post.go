651a41919d02735b3721f7c50203c5024e873825
hannes <hannes.study@gmail.com>
2024-06-19 12:51:15 +0300

    WIP with posts

--- pck/utils/post.go
+++ pck/utils/post.go
@@ -11,21 +11,18 @@ import (
 
 func RetrieveAllPosts() models.Posts {
 	var posts models.Posts
-
 	db, err := sql.Open("sqlite3", config.LION_DB)
 	if err != nil {
 		fmt.Println("error opening DB", err)
 		return posts
 	}
 	defer db.Close()
-
 	rows, err := db.Query("SELECT id, title, body, likes, dislikes, user_id, category_id, created_at, modified_at, active FROM posts")
 	if err != nil {
 		fmt.Println("Error querying DB:", err)
 		return posts
 	}
 	defer rows.Close()
-
 	for rows.Next() {
 		var post models.Post
 		err := rows.Scan(&post.ID, &post.Title, &post.Body, &post.Likes, &post.Dislikes, &post.UserId, &post.CategoryID, &post.CreatedAt, &post.ModifiedAt, &post.Active)
@@ -35,38 +32,44 @@ func RetrieveAllPosts() models.Posts {
 		}
 		posts.AllPosts = append(posts.AllPosts, post)
 	}
-
 	if err = rows.Err(); err != nil {
 		fmt.Println("Error during rows iteration:", err)
 	}
-
 	return posts
 }
 
-
-func FindPostsByUserName(userID string)models.Posts{
-
-
-return models.Posts{}
-}
-
-func FindPostByCategory( categoryName string) models.Posts {
-
-
-	
-return models.Posts{}
-}
-
-
+func FindPostsByUserName(userID string) models.Posts {
 //TODO: GetPostFromUser
+	return models.Posts{}
+}
+
+
+func FindPostByCategory(categoryName string) models.Posts {
 //TODO: GetPostFromCategory
-//TODO: GetAllPostLikes
-//TODO: GetAllPostDislikes no need to implement, if there is a counter you can just 
-//TODO: GetPostContaining
+
+	return models.Posts{}
+}
+
+func UpdatedComment(commentId int) {
+
+}
+
+
+
+func UpdateEmotes(emote string) {
+	//TODO: GetAllPostLikes
+	//TODO: GetAllPostDislikes no need to implement, if there is a counter you can just
+	//TODO: AddLike and RemoveLike
+	//TODO: Add dislike and RemoveDisLike
+
+}
+func CreateNewPost(post string, userName string){
 //TODO: AddUserPost
+}
+
+
+
+
+//TODO: FindPostContaining
+
 //? which rout should we go.
-//TODO: AddLike and RemoveLike
-//TODO: Add dislike and RemoveDisLike
-
-
-
