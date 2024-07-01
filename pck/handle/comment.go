f2a7b18aff5ab45eec1f6de4fc002a580524951c
karl-hendrikkahn <karlhendrikkahn@gmail.com>
2024-07-01 14:00:51 +0300

    Added redirecting back to previous page after commenting

--- pck/handle/comment.go
+++ pck/handle/comment.go
@@ -7,24 +7,24 @@ import (
 	"gitea.kood.tech/hannessoosaar/literary-lions/pck/utils"
 )
 
-
-func CommentHandler( w http.ResponseWriter, r *http.Request){
-	if !verifyPostMethod(w,r){
-	return
- 	}
+func CommentHandler(w http.ResponseWriter, r *http.Request) {
+	referer := r.Header.Get("Referer")
+	if !verifyPostMethod(w, r) {
+		return
+	}
 	verifiedUserName := verifySession(r)
-	if verifiedUserName == ""{
-		fmt.Printf("not a user log in" )
-		LandingPageHandler(w,r)
+	if verifiedUserName == "" {
+		fmt.Printf("not a user log in")
+		LandingPageHandler(w, r)
 	}
 	sessionUser := utils.FindUserByUserName(verifiedUserName)
 	postId := r.FormValue("postID")
 	fmt.Println("POST ID", postId)
 	if postId == "" {
-        http.Error(w, "Invalid post ID", http.StatusBadRequest)
-        return
-    }
+		http.Error(w, "Invalid post ID", http.StatusBadRequest)
+		return
+	}
 	comment := r.FormValue("comment")
-	utils.PostComment( sessionUser.ID, comment ,postId)
-	CategoryHandler(w,r)
+	utils.PostComment(sessionUser.ID, comment, postId)
+	http.Redirect(w, r, referer, http.StatusFound)
 }
