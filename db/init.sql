9ba7f3b5e670f62d0823906a00c26beadab875af
hannes <hannes.study@gmail.com>
2024-07-08 12:54:00 +0300

    Added ERD for lions.db

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS post_comments;
 DROP TABLE IF EXISTS user_activity;
 DROP TABLE IF EXISTS comment_replies;
 DROP TABLE IF EXISTS user_reply_activity;
